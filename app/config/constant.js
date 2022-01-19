2b2c99abe8f10acc3467048a0a00d7d3aef3e62f
Sadiyya Fayaz <Sadiyya@Fayazs-MacBook-Air.local>
2022-01-19 19:45:28 +0530

    Major project

---
+++ app/config/constant.js
@@ -0,0 +1,23 @@
+// Constant Data - Being used all over the backend!
+module.exports = {
+    emailSuffix     	: 'info.stanley.edu',
+    programs            : ['UG','M.Tech.','M.Plan.','M.Sc.','PhD','MBA'],
+    placementTeamRoles  : ['spc', 'faculty-coordinator'],
+    coordinatorData     : {
+        degree          : 'N/A',
+        department      : 'N/A',
+        program         : 'N/A',
+        cgpa            : 'N/A',
+        alternate_email : 'N/A',
+        address         : 'STANLEY Hyderabad',
+        city            : 'Hyderabad',
+        state           : 'Telanagana',
+        post_code       : '500057',
+        country         : 'India',
+        linkedln_link   : 'N/A',
+        matric_marks    : 'N/A',
+        matric_board    : 'N/A',
+        senior_marks    : 'N/A',
+        senior_board    : 'N/A'
+    }
+};
